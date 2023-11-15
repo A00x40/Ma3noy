778db3ac9ce44fdb7748b120fe52cdf3ff338612
A00x40 <Ahmedattia621@yahoo.com>
2023-11-15 21:26:30 +0200

    Added Quran & Song Pages

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from playsound import play_quran
+from playsound import sound, PlayList, play_quran, quran_list_dir, song_list_dir
 
 window_width = 600
 window_height = 500
@@ -32,7 +32,7 @@ class tkinterApp(tk.Tk):
 
         # for each page class create a new frame
         # with container as its parent
-        for fr in (StartPage,):
+        for fr in (StartPage, QuranPage, SongPage):
 
             frame = fr(container, self)
 
@@ -46,9 +46,9 @@ class tkinterApp(tk.Tk):
         frame = self.frames[container]
         frame.tkraise()
 
-
 # First window frame startpage
 class StartPage(tk.Frame):
+    
     def __init__(self, parent, controller):
         super().__init__(parent)
 
@@ -58,8 +58,8 @@ class StartPage(tk.Frame):
         BGlabel.image = img
         BGlabel.place(x=0, y=0, width=window_width, height=window_height)
 
-        #
-        self.playing = {"val":False}
+        
+        self.playing = {"val": False}
 
         # label (الصفحة الرئيسية)
         label = tk.Label(self, text="الصفحة الرئيسية", font=('Arial 18 bold'))
@@ -73,26 +73,154 @@ class StartPage(tk.Frame):
 
         quran_btn.place(relx=.8, rely=.3, anchor="se")
 
-        # # button (الذهاب لصفحة القرءان)
-        # quran_btn = tk.Button(self, text="القرءان", font=('Arial 14 bold'),
-        #                       width=10, height=1)
-        # quran_btn.place(relx=.8, rely=.45, anchor="se")
+        # button (الذهاب لصفحة القرءان)
+        quran_btn = tk.Button(self, text="القرءان", font=('Arial 14 bold'), width=10, height=1,
+                              command=lambda: controller.show_frame(QuranPage))
+        quran_btn.place(relx=.8, rely=.45, anchor="se")
 
-        # # button (الذهاب لصفحة الاغاني)
-        # song_btn = tk.Button(self, text="الأغاني", font=('Arial 14 bold'),
-        #                      width=10, height=1)
-        # song_btn.place(relx=.8, rely=.6, anchor="se")
+        # button (الذهاب لصفحة الاغاني)
+        song_btn = tk.Button(self, text="الأغاني", font=('Arial 14 bold'), width=10, height=1,
+                             command=lambda: controller.show_frame(SongPage))
+        song_btn.place(relx=.8, rely=.6, anchor="se")
 
-        # # button (الخروج من البرنامج)
-        # exit_btn = tk.Button(self, text="خروج", font=('Arial 14 bold'),
-        #                      width=10, height=1)
-        # exit_btn.place(relx=.8, rely=.75, anchor="se")
+        # button (الخروج من البرنامج)
+        exit_btn = tk.Button(self, text="خروج", font=('Arial 14 bold'), width=10, height=1,
+                             command=lambda: controller.destroy())
+        exit_btn.place(relx=.8, rely=.75, anchor="se")
 
+# Second window frame quranpage
+class QuranPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
 
+        # assigning a background image using PhotoImage
+        img = tk.PhotoImage(file="./assets/quran_bg.png")
+        BGlabel = tk.Label(self, image=img)
+        BGlabel.image = img
+        BGlabel.place(x=0, y=0, width=window_width, height=window_height)
+
+        # label (صفحة القرءان)
+        quran_label = tk.Label(self, text="صفحة القرءان", font=('Arial 18 bold'))
+        quran_label.place(relx=.5, rely=.1, anchor="center")
+
+        # button (العودة للصفحة الرئيسية)
+        quran_btn = tk.Button(self, text="عودة للرئيسية", font=('Arial 14 bold'), width=10, height=1,
+                              command=lambda: controller.show_frame(StartPage))
+        quran_btn.place(relx=.2, rely=.9, anchor="center")
+
+        # Creating a Listbox
+        listbox = PlayList(self)
+        listbox.config(bg="green", fg="black", width=45, font=('',12,'bold',))
+        listbox.place(relx=.5, rely=.35, anchor="center")
+        listbox.addList(quran_list_dir)
+
+        # Creating a Scrollbar and attaching it
+        scrollbar = tk.Scrollbar(self, orient="vertical") 
+  
+        # Adding Scrollbar to the right side 
+        scrollbar.pack(side = "right", fill = "y")
+
+        # Attaching Listbox to Scrollbar 
+        # Since we need to have a vertical scroll we use yscrollcommand 
+        listbox.config(yscrollcommand = scrollbar.set) 
+  
+        # setting scrollbar command parameter  
+        # to listbox.yview method its yview because 
+        # we need to have a vertical view 
+        scrollbar.config(command = listbox.yview)
+
+        # Action buttons 
+        play_btn = tk.Button(self, text="لعب", font=('Arial 14 bold'), width=7, height=1,
+                             command=lambda: listbox.play("sound/quran/"))
+        play_btn.place(relx=.4, rely=.6, anchor="center")
+
+        stop_btn = tk.Button(self, text="ايقاف", font=('Arial 14 bold'), width=7, height=1,
+                             command=lambda: listbox.stop())
+        stop_btn.place(relx=.6, rely=.6, anchor="center")
+
+        fromstart_btn = tk.Button(self, text="من البداية", font=('Arial 14 bold'), width=7, height=1,
+                             command=lambda: listbox.play("sound/quran/", 0))
+        fromstart_btn.place(relx=.2, rely=.6, anchor="center")
+
+        fromend_btn = tk.Button(self, text="من النهاية", font=('Arial 14 bold'), width=7, height=1,
+                             command=lambda: listbox.play("sound/quran/", listbox.itemcount - 1))
+        fromend_btn.place(relx=.8, rely=.6, anchor="center")
+
+        # button (الخروج من البرنامج)
+        exit_btn = tk.Button(self, text="خروج", font=('Arial 14 bold'), width=10, height=1,
+                             command=lambda: controller.destroy())
+        exit_btn.place(relx=.8, rely=.9, anchor="center")
+
+# Third window frame songpage
+class SongPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+
+        # assigning a background image using PhotoImage
+        img = tk.PhotoImage(file="./assets/start_bg.png")
+        BGlabel = tk.Label(self, image=img)
+        BGlabel.image = img
+        BGlabel.place(x=0, y=0, width=window_width, height=window_height)
+
+        # label (صفحة القرءان)
+        song_label = tk.Label(self, text="صفحة الأغاني", font=('Arial 18 bold'))
+        song_label.place(relx=.5, rely=.1, anchor="center")
+
+        # button (العودة للصفحة الرئيسية)
+        song_btn = tk.Button(self, text="عودة للرئيسية", font=('Arial 14 bold'), width=10, height=1,
+                              command=lambda: controller.show_frame(StartPage))
+        song_btn.place(relx=.2, rely=.9, anchor="center")
+
+        # Creating a Listbox
+        listbox = PlayList(self)
+        listbox.config(bg="green", fg="black", width=45, font=('',12,'bold',))
+        listbox.place(relx=.5, rely=.35, anchor="center")
+        listbox.addList(song_list_dir)
+
+        # Creating a Scrollbar and attaching it
+        scrollbar = tk.Scrollbar(self, orient="vertical") 
+  
+        # Adding Scrollbar to the right side 
+        scrollbar.pack(side = "right", fill = "y")
+
+        # Attaching Listbox to Scrollbar 
+        # Since we need to have a vertical scroll we use yscrollcommand 
+        listbox.config(yscrollcommand = scrollbar.set) 
+  
+        # setting scrollbar command parameter  
+        # to listbox.yview method its yview because 
+        # we need to have a vertical view 
+        scrollbar.config(command = listbox.yview)
+
+        # Action buttons 
+        play_btn = tk.Button(self, text="لعب", font=('Arial 14 bold'), width=7, height=1,
+                             command=lambda: listbox.play("sound/songs/"))
+        play_btn.place(relx=.4, rely=.6, anchor="center")
+
+        stop_btn = tk.Button(self, text="ايقاف", font=('Arial 14 bold'), width=7, height=1,
+                             command=lambda: listbox.stop())
+        stop_btn.place(relx=.6, rely=.6, anchor="center")
+
+        fromstart_btn = tk.Button(self, text="من البداية", font=('Arial 14 bold'), width=7, height=1,
+                             command=lambda: listbox.play("sound/songs/", 0))
+        fromstart_btn.place(relx=.2, rely=.6, anchor="center")
+
+        fromend_btn = tk.Button(self, text="من النهاية", font=('Arial 14 bold'), width=7, height=1,
+                             command=lambda: listbox.play("sound/songs/", listbox.itemcount - 1))
+        fromend_btn.place(relx=.8, rely=.6, anchor="center")
+
+        # button (الخروج من البرنامج)
+        exit_btn = tk.Button(self, text="خروج", font=('Arial 14 bold'), width=10, height=1,
+                             command=lambda: controller.destroy())
+        exit_btn.place(relx=.8, rely=.9, anchor="center")
 def main():
     app = tkinterApp()
 
     app.mainloop()
+
+    # check to end music thread
+    if sound.mixer.music.get_busy():
+        sound.stop()
 
 
 if __name__ == "__main__":
--- playsound/__init__.py
+++ playsound/__init__.py
@@ -1,34 +1,99 @@
 import os
 from pathlib import Path
+from tkinter import Listbox
 import random
 import pygame
+from threading import Thread
+from time import sleep
 
 class SoundApp():
+
+    music_thread = None
 
     def __init__(self):
         self.mixer = pygame.mixer
         self.mixer.init()# initialise the pygame
+        self.stop_playing = True
  
-    def playAll(self, file_list):
+    def playAll(self, file_list, playlist=None):
 
-        self.mixer.music.load(file_list[0])
-        for i in range(1, len(file_list)):
-            self.mixer.music.queue(file_list[i])
-        self.mixer.music.play()
+        self.stop_playing = False
+        for i in range(0, len(file_list)):
+            
+            if self.stop_playing:
+                break
 
+            self.mixer.music.load(file_list[i])
+            self.mixer.music.play()
+
+            while self.mixer.music.get_busy():
+                continue
+
+            # After song ends move selection tto next
+            if playlist != None :
+                selection = playlist.curselection()[0]
+
+                print(selection , playlist.itemcount)
+
+                if (selection+1) < playlist.itemcount:
+                    playlist.selection_clear(selection)
+                    playlist.selection_set(selection+1)
+          
     def pause(self):
         self.mixer.music.pause()
+        
+        
 
     def stop(self):
         self.mixer.music.stop()
+        self.stop_playing = True
 
 sound = SoundApp()
-
 
 quran_list_dir = os.listdir(f"{Path(__file__).parent.parent}/sound/quran") 
 quran_list = []
 for filename in quran_list_dir:
     quran_list.append(os.path.join("sound/quran/", filename))
+
+song_list_dir = os.listdir(f"{Path(__file__).parent.parent}/sound/songs") 
+
+# Playlist class
+class PlayList(Listbox):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.itemcount = 0
+
+    def addList(self, items):
+        for i in range(len(items)):
+            self.insert(i, items[i])
+            self.itemcount += 1
+
+    # position: 0 -> from start, (itemcount - 1) -> from end
+    def play(self, path, position=-1):
+
+        selected = self.curselection()
+
+        if position != -1:
+            if len(selected) != 0:
+                self.selection_clear(selected)
+            self.selection_set(position)
+            selected = [position]
+            
+        # check if item is selected
+        if len(selected) != 0:
+            index = selected[0]
+
+            filelist = []
+            for i in range(index, self.itemcount):
+                filelist.append(f"{path}{self.get(i)}")
+            
+            if sound.mixer.music.get_busy():
+                sound.stop()
+            sound.music_thread = Thread(target=sound.playAll, args=(filelist, self,), daemon=True)
+            sound.music_thread.start()
+
+    def stop(self):
+        sound.stop()
 
 def play_quran(is_playing):
    
@@ -37,7 +102,11 @@ def play_quran(is_playing):
         is_playing["val"] = True
         
         random.shuffle(quran_list)
-        sound.playAll(quran_list)
+
+        # creatte a thread to run music in background
+        # daemon option terminates the thread when the program closes
+        sound.music_thread = Thread(target=sound.playAll, args=(quran_list,), daemon=True)
+        sound.music_thread.start()
             
     # stop playing sound 
     else:
